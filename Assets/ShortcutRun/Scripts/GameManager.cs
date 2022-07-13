0ca7d1eb60987115060418742ad77358c96883df
fpgl_shamit <62556347+shamitfpgl@users.noreply.github.com>
2022-07-13 14:50:39 +0600

    stack wip

--- Assets/ShortcutRun/Scripts/GameManager.cs
+++ Assets/ShortcutRun/Scripts/GameManager.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    [Header("Stacking")]
+    public GameObject logPickupObj;
+    public GameObject logStackObj;
+    public int curStackCount;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/ShortcutRun/Scripts/PlayerCollisions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("logpickup"))
+        {
+            Destroy(other.gameObject);
+            GameManager.instance.curStackCount++;
+
+        }   
+    }
+}
