95140c1e0112227f7d4aac3e71471a665497c5eb
fpgl_shamit <62556347+shamitfpgl@users.noreply.github.com>
2022-07-14 12:10:17 +0600

    sound wip

--- Assets/ShortcutRun/Scripts/SoundManager.cs
+++ Assets/ShortcutRun/Scripts/SoundManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance;
+
+    public bool soundOn;
+    public AudioSource sfxAuidoSource;
+    public GameObject bgMusicIngame;
+    public List<AudioClip> collisionSFX;
+    public AudioClip splashSFX;
+    public AudioClip buttonTapSFX;
+    //public AudioClip collisionSFX;
+    public AudioClip tapStartSFX;
+    public AudioClip coinSFX;
+    public AudioClip bounceSFX;
+    public AudioClip winSFX;
+    public AudioClip loseSFX;
+    public AudioClip logPickSFX;
+    public AudioClip logPlaceSFX;
+    public AudioClip jumpSFX;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        soundOn = true;
+        sfxAuidoSource = GetComponent<AudioSource>();
+        //bgMusicIngame.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySFX(AudioClip audioClip)
     {
-        
+        //if (PlayerPrefs.GetInt("isSound") == 1)
+        //{
+        if (soundOn)
+        {
+            sfxAuidoSource.PlayOneShot(audioClip);
+        }
+
+        //}
+    }
+
+    public AudioClip GetRandomHitSFX()
+    {
+        return collisionSFX[Random.Range(0, collisionSFX.Count)];
     }
 }
