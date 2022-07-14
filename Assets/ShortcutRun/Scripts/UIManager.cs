ec3bdef41dd5ddcac4890365bdb08dde9c916515
fpgl_shamit <62556347+shamitfpgl@users.noreply.github.com>
2022-07-14 23:24:54 +0600

    before haptic

--- Assets/ShortcutRun/Scripts/UIManager.cs
+++ Assets/ShortcutRun/Scripts/UIManager.cs
@@ -45,6 +45,7 @@ public class UIManager : MonoBehaviour
     public void BtnStart()
     {
         panelStart.SetActive(false);
+        
         StartCoroutine(StartRoutine());
     }
     public void BtnRetryCallback()
